c2ab164b43311ec0a7143c31d25d4d1d270fe35d
Memeathon <rafilalam009@gmail.com>
2026-01-02 14:00:58 +0800

    My First Triangle

--- Renderer/main.cpp
+++ Renderer/main.cpp
@@ -5,5 +5,64 @@
 int main(void)
 {
 	if (!glfwInit())
-		std::cout << "GLFW Failed to initialise!" << std::endl;
+	{
+		std::cerr << "GLFW Failed to initialise!" << std::endl; 
+		return -1;
+	}
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "Renderer", NULL, NULL);
+	if (!window)
+	{
+		std::cerr << "GLFW Failed to create a window!" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+	
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cerr << "GLAD Failed to initialise!" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	float vertices[9] = {
+		-0.5f, -0.5f, 0.0f,
+		0.0f, 0.5f, 0.0f,
+		0.5f, -0.5f, 0.0f
+	};
+
+	GLuint VAO, VBO;
+
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+
+	glBindVertexArray(VAO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), &vertices, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	initShaders();
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glClearColor(0.02f, 0.07f, 0.2f, 0.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	std::cout << "GLFW Window Closed!" << std::endl;
+	glfwTerminate();
+	return 0;
 }
