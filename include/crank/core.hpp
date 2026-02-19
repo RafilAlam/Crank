106b18a1239fd0ce2105006c726373644910779e
Rafil Alam <rafilalam009@gmail.com>
2026-02-19 19:15:25 +0800

    added shader, shaderprogram and partially complete window class

--- include/crank/core.hpp
+++ include/crank/core.hpp
@@ -2,4 +2,55 @@
 
 #include <iostream>
 #include <fstream>
+#include <string>
 #include <vector>
+#include <cstdlib>
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+namespace crank {
+
+class Shader {
+private:
+  const char* fetchShader(std::string fileName) {
+    std::ifstream file(fileName);
+    if (!file)
+      std::cerr << "FAILED TO LOAD SHADER: " << fileName;
+    
+    std::string content;
+    std::string line;
+    while (std::getline(file, line)) {
+      content += line;
+    }
+    const char* ccontent = content.c_str();
+
+    return ccontent;
+  }
+public:
+  const GLuint handle;
+  Shader(std::string fileName): handle(glCreateShader());
+}
+
+class ShaderProgram {
+private:
+public:
+  const GLuint handle;
+  ShaderProgram();
+  AttachShader();
+  Link();
+}
+
+class Window {
+private:
+public:
+  Window();
+}
+
+class RenderingPipeline {
+private:
+public:
+  
+}
+
+}
--- src/crank/core.cpp
+++ src/crank/core.cpp
@@ -1,7 +1,29 @@
-#include <iostream>
-
 #include <crank/core.hpp>
 
-int main() {
-  std::cout << "Hello, World!" << std::endl;
+namespace crank {
+  Shader::Shader(std::string fileName): handle(glCreateShader()) {
+    const char* shadercontent = fetchShader(fileName);
+    glShaderSource(handle, 1, &shadercontent, NULL);
+    glCompileShader(handle);
+  }
+
+  ShaderProgram::ShaderProgram(): handle(glCreateProgram()) {}
+  ShaderProgram::AttachShader(GLuint shader) {
+    glAttachShader(handle, shader.handle);
+  }
+  ShaderProgram::Link() {
+    glLinkProgram(handle);
+  }
+
+  Window::Window() {
+    if (!glfwInit()) {
+      std::cout << "GLFW couldn't start" << std::endl;
+      std::exit(EXIT_FAILURE);
+    }
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+
+  }
 }
