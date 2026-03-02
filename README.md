1088034bfaf5ee16eec9c694e4c8b9e227612e61
Rafil Alam <rafilalam009@gmail.com>
2026-03-02 18:58:36 +0800

    ...

--- README.md
+++ README.md
@@ -21,3 +21,44 @@ cmake --build build
 </code>
 </pre>
 <p>Application code can be found in the app directory.</p>
+
+<h2>Example Crank App</h2>
+<p>The code below is a basic example of an application that shows a spinning square, triangle and a circle.</p>
+<pre>
+<code>
+#include <iostream>
+#include <vector>
+
+#include <crank/core.hpp>
+using namespace crank;
+
+int frames{0};
+
+int main() {
+  Window Window("Crank", 800, 600);
+  Renderer2D Renderer(Window);
+  Object& rectangle = Renderer.Create("Rectangle", Mesh::Rectangle(100, 100));
+  Object& triangle = Renderer.Create("", Mesh::Triangle(100, 100));
+  Object& circle = Renderer.Create("Circle", Mesh::Circle(50));
+
+  rectangle.color = glm::vec3(0.5f, 0.1f, 0.1f);
+  triangle.color = glm::vec3(0.1f, 0.5f, 0.1f);
+  circle.color = glm::vec3(0.1f, 0.1f, 0.5f);
+
+  rectangle.transform.position = glm::vec3(200.0f, 200.0f, 0.0f);
+  triangle.transform.position = glm::vec3(350.0f, 200.0f, 0.0f);
+  circle.transform.position = glm::vec3(500.0f, 200.0f, 0.0f);
+
+  Window.Keybind(GLFW_KEY_E, []() { std::cout << "KEY 'E' PRESSED!" << std::endl; });
+
+  Renderer.PreRenderStep = [&]() {
+    rectangle.transform.rotation.z += 0.1;
+    triangle.transform.rotation.z += 0.1;
+    circle.transform.rotation.z += 0.1;
+  };
+
+  Renderer.Run();
+  return 0;
+}
+</code>
+</pre>
--- app/main.cpp
+++ app/main.cpp
@@ -9,21 +9,24 @@ int frames{0};
 int main() {
   Window Window("Crank", 800, 600);
   Renderer2D Renderer(Window);
-  Object& circle = Renderer.Create("Circle", Mesh::Circle(100));
-  circle.color = glm::vec3(0.1f, 0.1f, 0.5f);
+  Object& rectangle = Renderer.Create("Rectangle", Mesh::Rectangle(100, 100));
   Object& triangle = Renderer.Create("", Mesh::Triangle(100, 100));
-  triangle.color = glm::vec3(0.8f, 0.2f, 0.2f);
+  Object& circle = Renderer.Create("Circle", Mesh::Circle(50));
 
+  rectangle.color = glm::vec3(0.5f, 0.1f, 0.1f);
+  triangle.color = glm::vec3(0.1f, 0.5f, 0.1f);
+  circle.color = glm::vec3(0.1f, 0.1f, 0.5f);
+
+  rectangle.transform.position = glm::vec3(200.0f, 200.0f, 0.0f);
+  triangle.transform.position = glm::vec3(350.0f, 200.0f, 0.0f);
   circle.transform.position = glm::vec3(500.0f, 200.0f, 0.0f);
-  //circle.transform.scale = glm::vec3(100.0f, 100.0f, 1.0f);
-  triangle.transform.position = glm::vec3(200.0f, 200.0f, 0.0f);
-  //triangle.transform.scale = glm::vec3(100.0f, 100.0f, 100.0f);
 
   Window.Keybind(GLFW_KEY_E, []() { std::cout << "KEY 'E' PRESSED!" << std::endl; });
 
   Renderer.PreRenderStep = [&]() {
+    rectangle.transform.rotation.z += 0.1;
+    triangle.transform.rotation.z += 0.1;
     circle.transform.rotation.z += 0.1;
-    triangle.transform.rotation.z -= 0.1;
   };
 
   Renderer.Run();
