d170a80235291fe5ac8be3776d5f405e2af75123
Rafil Alam <rafilalam009@gmail.com>
2026-03-02 17:48:10 +0800

    Created README

--- app/main.cpp
+++ app/main.cpp
@@ -10,9 +10,9 @@ int main() {
   Window Window("Crank", 800, 600);
   Renderer2D Renderer(Window);
   Object& circle = Renderer.Create("Circle1", Mesh::Circle(100));
-  circle.color = glm::vec3(0.5f, 0.2f, 0.2f);
+  circle.color = glm::vec3(0.1f, 0.1f, 0.5f);
   Object& triangle = Renderer.Create("Triangle2", Mesh::Triangle(100, 100));
-  triangle.color = glm::vec3(0.2f, 0.5f, 0.2f);
+  triangle.color = glm::vec3(0.8f, 0.2f, 0.2f);
 
   circle.transform.position = glm::vec3(500.0f, 200.0f, 0.0f);
   //circle.transform.scale = glm::vec3(100.0f, 100.0f, 1.0f);
