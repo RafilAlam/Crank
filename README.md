45d769d573b703b2d30a0613a004024dfd86865f
Rafil Alam <rafilalam009@gmail.com>
2026-03-02 18:39:32 +0800

    Added installation to README

--- README.md
+++ README.md
@@ -2,3 +2,21 @@
 <p><strong>Crank</strong> is an OpenGL Graphics Engine written in the C++ language.
 It can be used to quickly write cross-platform C++ applications including not just games, 
 but simulations, graph visualisations and tech showcases. </p>
+
+<h2> Installation: </h2>
+<p> Crank uses CMake as its build system. </p>
+<pre>
+<code>
+cd path/to/crank/directory
+
+mkdir build
+
+cd build
+
+cmake ..
+
+cd ..
+
+cmake --build build
+</code>
+<p>Application code can be found in the app directory.</p>
--- app/main.cpp
+++ app/main.cpp
@@ -9,9 +9,9 @@ int frames{0};
 int main() {
   Window Window("Crank", 800, 600);
   Renderer2D Renderer(Window);
-  Object& circle = Renderer.Create("Circle1", Mesh::Circle(100));
+  Object& circle = Renderer.Create("Circle", Mesh::Circle(100));
   circle.color = glm::vec3(0.1f, 0.1f, 0.5f);
-  Object& triangle = Renderer.Create("Triangle2", Mesh::Triangle(100, 100));
+  Object& triangle = Renderer.Create("", Mesh::Triangle(100, 100));
   triangle.color = glm::vec3(0.8f, 0.2f, 0.2f);
 
   circle.transform.position = glm::vec3(500.0f, 200.0f, 0.0f);
