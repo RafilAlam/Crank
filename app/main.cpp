566990d418cdb932befd77718a72c07e66518697
Rafil Alam <rafilalam009@gmail.com>
2026-03-06 14:29:56 +0800

    UNSTABLE: fixed sdf, model positioning broken

--- app/main.cpp
+++ app/main.cpp
@@ -10,9 +10,9 @@ int main() {
   Object& rectangle = Renderer.Create("Rectangle", Mesh::Rectangle(100, 100));
  
   rectangle.color = glm::vec3(0.5f, 0.1f, 0.1f);
-  rectangle.cornerradius = 100.0f;
+  rectangle.cornerradius = 10.0f;
 
-  rectangle.transform.position = glm::vec3(200.0f, 200.0f, 0.0f);
+  rectangle.transform.position = glm::vec3(0.0f, 0.0f, 0.0f);
 
   Window.Keybind(GLFW_KEY_E, []() { std::cout << "KEY 'E' PRESSED!" << std::endl; });
 
--- shaders/fragment.glsl
+++ shaders/fragment.glsl
@@ -10,21 +10,21 @@ in vec4 gl_FragCoord;
 out vec4 FragColor;
 
 uniform vec2 u_modelposition;
-uniform vec2 u_meshsize;
+uniform vec2 u_modelsize;
 uniform float u_cornerradius;
 uniform vec3 u_color;
 uniform int u_meshtype;
 uniform vec2 u_resolution;
 
-float sdfRect(vec2 point, vec2 halfsize)
+float sdfRect(vec2 point, vec2 halfsize) 
 {
-  vec2 q = abs(point) - halfsize*0.5;
-  return length(max(q, 0.0)) + min(max(q.x, q.y), 0.0);
+  vec2 q = abs(point) - halfsize;
+  return length(max(q, 0.0f)) + min(max(q.x, q.y), 0.0);
 }
 
 void main()
 {
-  float d = sdfRect(gl_FragCoord.xy - u_modelposition, u_meshsize - u_cornerradius) - u_cornerradius;
+  float d = sdfRect(gl_FragCoord.xy - u_modelposition, u_modelsize*0.5 - u_cornerradius) - u_cornerradius;
   float alpha = smoothstep(1.0, -1.0, d);
   FragColor = vec4(u_color, alpha);
 }
